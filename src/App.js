d8c215e2226b5712133fcc32ad4021dbb4a2472c
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-12 09:47:28 +0000

    Needs Editing of images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Banner.css';
+function Banner(){
+    return(
+        <div className="Banner">
+            <div className="Text">
+                <div className="Heading">
+                    Computer Engineering
+                </div>
+                <div className="Subheading">
+                    142,765 Computer Engineers follow this
+                </div>
+            </div>
+        </div>
+    )
+};
+export default Banner;

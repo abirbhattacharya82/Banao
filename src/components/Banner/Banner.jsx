0819a3ccc634052ace29fcef18f2985deb2576c7
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-04-12 17:20:10 +0000

    Banner Done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import './Banner.css';
-function Banner(){
-    return(
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+function Banner() {
+    return (
         <div className="Banner">
             <div className="Text">
+                <div className="ButtonAreaMobile">
+                    <div className="Left">
+                        <button><FontAwesomeIcon icon={faArrowLeft} /></button>
+                    </div>
+                    <div className="Right">
+                        <button>Join Group</button>
+                    </div>
+                </div>
                 <div className="Heading">
                     Computer Engineering
                 </div>
